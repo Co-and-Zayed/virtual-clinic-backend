08625e7e125998390515cd3120eec992401733fd
Seif Hany <seif.naguib@gmail.com>
2023-10-09 13:03:09 +0300

    comment

--- src/routes/auth/authController.js
+++ src/routes/auth/authController.js
@@ -78,7 +78,7 @@ async function authenticateToken(req, res, next) {
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     console.log(err);
-    if (err) return res.sendStatus(403);
+    if (err) return res.sendStatus(403); // should send a refresh request
     req.user = user;
     next();
   });
