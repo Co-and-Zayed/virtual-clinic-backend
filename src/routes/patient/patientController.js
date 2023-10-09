aa142067bde954e53fbe808c9a50b8e3c68e393f
Seif Hany <seif.naguib@gmail.com>
2023-10-09 22:00:10 +0300

    mashy

--- src/routes/patient/patientController.js
+++ src/routes/patient/patientController.js
@@ -62,10 +62,10 @@ const getDoctors = async (req, res) => {
 ///////////
 const getDoctordetails = async (req, res) => {
   // view doctor details by selecting the name.
-  const name = req.body.name;
+  const username = req.body.username;
 
   try {
-    const doctor = await doctorModel.find({ name: name });
+    const doctor = await Doctor.find({ username });
 
     res.status(200).json(doctor);
   } catch (err) {
