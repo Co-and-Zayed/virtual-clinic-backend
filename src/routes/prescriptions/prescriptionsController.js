f578b9155144eefac6ea8cd2d39ac5515f35b340
zeina2 <refaizeina8@gmail.com>
2023-10-10 23:45:45 +0300

    be

--- src/routes/prescriptions/prescriptionsController.js
+++ src/routes/prescriptions/prescriptionsController.js
@@ -13,7 +13,7 @@ const getAllPrescriptions = async (req, res) => {
 
 //get a single prescription
 const getSinglePrescription = async (req, res) => {
-  try {
+try {
     const prescription = await prescriptionsModel.findById(req.params.id);
     res.status(200).json(prescription);
   } catch (err) {
